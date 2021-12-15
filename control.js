ddd460c6431e4bfb837d4f75dcac29c16d73780f
Eli Weber <eliweber2001@gmail.com>
2021-12-15 17:04:08 -0500

    JS swiping capability implemented

--- control.js
+++ control.js
@@ -9,6 +9,73 @@ window.addEventListener("DOMContentLoaded", function(e) {
 
 }, false);
 
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            var path = window.location.pathname;
+            var page = path.split("/").pop();
+            if(page === "schedule.html")
+            {
+                slideShow(12, '#innerContainer', '.slide')
+            }
+            if(page === "therapy.html")
+            {
+                slideShow2(12, '#innerContainer2', '.slider')
+            }
+            
+        } else {
+            var path = window.location.pathname;
+            var page = path.split("/").pop();
+            if(page === "schedule.html")
+            {
+                slideShow(1, '#innerContainer', '.slide')
+            }
+            if(page === "therapy.html")
+            {
+                slideShow2(1, '#innerContainer2', '.slider')
+            }
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
 function pageLoaded()
 {
     let body = document.body;
