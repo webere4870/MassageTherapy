e8378fad6211d72efb15aef4cb9df1295eadf1ee
Eli Weber <eliweber2001@gmail.com>
2022-03-07 17:28:19 -0500

    Home page slideshow added

---
+++ home.js
@@ -0,0 +1,20 @@
+let counter = 0;
+$('.linkImg').click((evt)=>
+        {
+            evt.preventDefault();
+            $('body').css("background", "linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)), url(" + $(evt.currentTarget).attr("href") + ")")
+            $('body').css("background-position", "center")
+            $('body').css("background-size", "cover")
+            $("body").css("background-repeat", "no-repeat")
+            $("body").css("background-attachment", "scroll")
+        })
+
+setInterval(()=>
+{
+    $('body').css("background", "linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)), url(" + $(`.linkImg:nth-of-type(${counter + 1})`).attr("href") + ")")
+            $('body').css("background-position", "center")
+            $('body').css("background-size", "cover")
+            $("body").css("background-repeat", "no-repeat")
+            $("body").css("background-attachment", "scroll")
+    counter = (counter + 1) % 4;
+}, 3000)
