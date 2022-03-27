22d3b0ae38af350330407c9427155b6a559201b2
Eli Weber <eliweber2001@gmail.com>
2022-03-27 14:54:43 -0400

    Testing PHP form

--- form.php
+++ form.php
@@ -1,10 +1,14 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
+<?php
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = $_POST['email'];
+$to = "ryan@rdmtherapy.com";
+$subject = $_POST['name'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "eliweber2001@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+$headers = "From:" . $from;
+if(mail($to,$subject,$message, $headers)) {
+    echo "The email message was sent.";
+} else {
+    echo "The email message was not sent.";
+}
 ?>
