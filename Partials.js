7e6931aaa548a0eb13c2baa8a3709e5058efac4e
Eli Weber <eliweber2001@gmail.com>
2022-11-06 17:27:38 -0500

    Nav

--- Partials.js
+++ Partials.js
@@ -1,23 +1,19 @@
 let nav=`
 <nav>
-        <a href="./index.html">Home</a>
-        <a href="./history.html">History</a>
-        <a href="./doctrine.html">Doctrine</a>
-        <a href="./info.html">Info</a>
-        <a href="./contact.html">Contact</a>
-        <a href="./calendar.html">Events</a>
+<a href="./index.html" class="swipeInDude">Home</a>
+<a href="./schedule.html" class="swipeInDude">Schedule</a>
+<a href="./about.html" class="swipeInDude">About</a>
+<a href="./therapy.html" class="swipeInDude">Treatments</a>
 </nav>
 
 <input type="checkbox" id="active">
 <label for="active" class="menu-btn"><i style="color: black" class="fas fa-bars"></i></label>
 <div class="wrapper">
     <ul>
-        <li><a href="./index.html">Home</a></li>
-        <li><a href="./history.html">History</a></li>
-        <li><a href="./doctrine.html">Doctrine</a></li>
-        <li><a href="./info.html">Info</a></li>
-        <li><a href="./contact.html">Contact</a></li>
-        <li><a href="./contact.html">Events</a></li>
+    <li><a href="./index.html" class="swipeInDude">Home</a><li/>
+    <li><a href="./schedule.html" class="swipeInDude">Schedule</a><li/>
+    <li><a href="./about.html" class="swipeInDude">About</a><li/>
+    <li><a href="./therapy.html" class="swipeInDude">Treatments</a><li/>
     </ul>
 </div> `
 
